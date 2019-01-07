f9dc7f241c370d4794dfaeabf0f9e81c5ef913d3
hyuan <h_y_12@163.com>
2019-01-07 15:23:24 +0800

    添加前台框架代码
    Signed-off-by: hyuan <h_y_12@163.com>

--- issoftstone-auth-core/src/main/java/com/iss/oauth/password/UsernamePasswordAuthentication.java
+++ issoftstone-auth-core/src/main/java/com/iss/oauth/password/UsernamePasswordAuthentication.java
@@ -19,73 +19,73 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 public class UsernamePasswordAuthentication extends UsernamePasswordAuthenticationFilter {
 
-	// 是否开启验证码功能
-	private boolean isOpenValidateCode = true;
+    // 是否开启验证码功能
+    private boolean isOpenValidateCode = true;
 
-	public static final String VALIDATE_CODE = "verifyCode";
+    public static final String VALIDATE_CODE = "verifyCode";
 
-	public UsernamePasswordAuthentication() {
-		SimpleUrlAuthenticationFailureHandler failedHandler = (SimpleUrlAuthenticationFailureHandler) getFailureHandler();
-		failedHandler.setDefaultFailureUrl("/login.jsp?error=2");
-		this.setAuthenticationManager(getAuthenticationManager());
-	}
+    public UsernamePasswordAuthentication() {
+        SimpleUrlAuthenticationFailureHandler failedHandler = (SimpleUrlAuthenticationFailureHandler) getFailureHandler();
+        failedHandler.setDefaultFailureUrl("/login.jsp?error=2");
+        this.setAuthenticationManager(getAuthenticationManager());
+    }
 
-	@Override
-	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
-			throws AuthenticationException {
-		String verification = request.getParameter("code");
-		String captcha = request.getSession().getAttribute(VALIDATE_CODE).toString();
-		if (!captcha.contentEquals(verification)) {
-			try {
-				throw new Exception("captcha code not matched!");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return super.attemptAuthentication(request, response);
-	}
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        String verification = request.getParameter("code");
+        String captcha = request.getSession().getAttribute(VALIDATE_CODE).toString();
+        if (!captcha.contentEquals(verification)) {
+            try {
+                throw new Exception("captcha code not matched!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return super.attemptAuthentication(request, response);
+    }
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpServletResponse res = (HttpServletResponse) response;
-		if (!requiresAuthentication(req, res)) {
-			chain.doFilter(request, response);
-			return;
-		}
-		if (isOpenValidateCode) {
-			if (!checkValidateCode(req, res)) {
-				return;
-			}
-		}
-		chain.doFilter(request, response);
-	}
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        if (!requiresAuthentication(req, res)) {
+            chain.doFilter(request, response);
+            return;
+        }
+        if (isOpenValidateCode) {
+            if (!checkValidateCode(req, res)) {
+                return;
+            }
+        }
+        chain.doFilter(request, response);
+    }
 
-	protected boolean checkValidateCode(HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		HttpSession session = request.getSession();
-		String sessionValidateCode = obtainSessionValidateCode(session);
-		// 让上一次的验证码失效
-		session.setAttribute(VALIDATE_CODE, null);
-		String validateCodeParameter = obtainValidateCodeParameter(request);
-		if (StringUtils.isEmpty(validateCodeParameter)
-				|| !sessionValidateCode.equalsIgnoreCase(validateCodeParameter)) {
-			unsuccessfulAuthentication(request, response, new InsufficientAuthenticationException("输入的验证码不正确"));
-			return false;
-		}
-		// 保存一些session信息
-		session.setAttribute(VALIDATE_CODE, validateCodeParameter);
-		return true;
-	}
+    protected boolean checkValidateCode(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        HttpSession session = request.getSession();
+        String sessionValidateCode = obtainSessionValidateCode(session);
+        // 让上一次的验证码失效
+        session.setAttribute(VALIDATE_CODE, null);
+        String validateCodeParameter = obtainValidateCodeParameter(request);
+        if (StringUtils.isEmpty(validateCodeParameter)
+                || !sessionValidateCode.equalsIgnoreCase(validateCodeParameter)) {
+            unsuccessfulAuthentication(request, response, new InsufficientAuthenticationException("输入的验证码不正确"));
+            return false;
+        }
+        // 保存一些session信息
+        session.setAttribute(VALIDATE_CODE, validateCodeParameter);
+        return true;
+    }
 
-	private String obtainValidateCodeParameter(HttpServletRequest request) {
-		Object obj = request.getParameter(VALIDATE_CODE);
-		return null == obj ? "" : obj.toString();
-	}
+    private String obtainValidateCodeParameter(HttpServletRequest request) {
+        Object obj = request.getParameter(VALIDATE_CODE);
+        return null == obj ? "" : obj.toString();
+    }
 
-	protected String obtainSessionValidateCode(HttpSession session) {
-		Object obj = session.getAttribute(VALIDATE_CODE);
-		return null == obj ? "" : obj.toString();
-	}
+    protected String obtainSessionValidateCode(HttpSession session) {
+        Object obj = session.getAttribute(VALIDATE_CODE);
+        return null == obj ? "" : obj.toString();
+    }
 }
--- issoftstone-orm/src/main/java/com/iss/orm/factory/DefaultRepositoryFactoryBean.java
+++ issoftstone-orm/src/main/java/com/iss/orm/factory/DefaultRepositoryFactoryBean.java
@@ -13,9 +13,13 @@ import org.springframework.data.repository.core.support.RepositoryFactorySupport
 import com.iss.orm.repository.CustomRepostiory;
 import com.iss.orm.repository.impl.CustomRepostioryImpl;
 
+/**
+ * @author Administrator
+ */
 public class DefaultRepositoryFactoryBean<R extends JpaRepository<T, I>, T, I extends Serializable> extends
 		JpaRepositoryFactoryBean<R, T, I> {
 	
+	@Override
 	protected RepositoryFactorySupport createRepositoryFactory(EntityManager entityManager) {
 		return new DynamicQueryRepositoryFactory<>(entityManager);
 	}
@@ -23,15 +27,17 @@ public class DefaultRepositoryFactoryBean<R extends JpaRepository<T, I>, T, I ex
 	private static class DynamicQueryRepositoryFactory<T, I extends Serializable> extends JpaRepositoryFactory {
 		private EntityManager entityManager;
 
-		public DynamicQueryRepositoryFactory(EntityManager entityManager) {
+		DynamicQueryRepositoryFactory(EntityManager entityManager) {
 			super(entityManager);
 			this.entityManager = entityManager;
 		}
 		
+		@Override
 		protected Object getTargetRepository(RepositoryMetadata metadata) {
 			return new CustomRepostioryImpl<>(metadata.getDomainType(), this.entityManager);
 		}
 
+		@Override
 		protected Class<?> getRepositoryBaseClass(RepositoryMetadata metadata) {
 			return CustomRepostiory.class;
 		}
