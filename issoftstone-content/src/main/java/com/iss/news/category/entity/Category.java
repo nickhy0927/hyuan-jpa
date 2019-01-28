75039cfea6c0f32ac7e156831f91916b90aa00c8
hyuan <h_y_12@163.com>
2019-01-28 14:35:47 +0800

    添加内容模块

---
+++ issoftstone-content/src/main/java/com/iss/news/category/entity/Category.java
@@ -0,0 +1,58 @@
+package com.iss.news.category.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.iss.common.utils.IdEntity;
+
+@Entity
+@Table(name = "t_d_category")
+public class Category extends IdEntity {
+
+	/**
+	 * 栏目编号
+	 */
+	private String code;
+
+	/**
+	 * 栏目名称
+	 */
+	private String name;
+
+	/**
+	 * 上级分类
+	 */
+	private Category category;
+	
+	@Column(columnDefinition = "varchar(64) comment '栏目编号'")
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	@Column(columnDefinition = "varchar(64) comment '栏目名称'")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "p_id", columnDefinition = "varchar(255) comment '上级栏目'")
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+}
